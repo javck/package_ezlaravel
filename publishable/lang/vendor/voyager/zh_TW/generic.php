0f6b376a6cd386c41bdfd42876414f640fe837b9
Lin Pin Chueh <javck.zack@gmail.com>
2019-06-28 21:33:58 +0800

    加入語系檔

---
+++ publishable/lang/vendor/voyager/zh_TW/generic.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'no_results'             => '',
+    'bulk_create_confirm' 	=> '合成訂單',
+    'bulk-create-order'		=> '將選定集運單合成訂單',
+    'username' 				=> '帳號',
+    'password'				=> '密碼',
+    'developer_guide' 		=> '開發者手冊',
+    'developer_desc' 		=> '開發者實用資訊，含Layout與API',
+    'canvas_guide' 			=> 'Canvas手冊',
+    'canvas_desc' 			=> 'Canvas實用資訊，含CSS與工具',
+    'order'					=> '順序',
+    'home'					=> '返回首頁',
+];
+

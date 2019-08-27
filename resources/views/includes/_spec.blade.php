2624e32d509ef7e72e3348f9ae064d5dca42ef06
Lin Pin Chueh <javck.zack@gmail.com>
2019-08-27 16:38:55 +0800

    修改_spec

--- resources/views/includes/_spec.blade.php
+++ resources/views/includes/_spec.blade.php
@@ -1,11 +1,11 @@
 <div class="container clearfix">
 
-    <div class="heading-block center" data-animate="@if(isset($item_spec_pageTop->animation)){{$item_spec_pageTop->animation}}@endif" data-delay="@if(isset($item_spec_pageTop->animationDelay)){{$item_spec_pageTop->animationDelay}}@endif">
-        <h2>{{ $item_spec_pageTop->title }}<span>{{ $item_spec_pageTop->subtitle }}</span></h2>
-        <span>{!! $item_spec_pageTop->content !!}</span>
+    <div class="heading-block center" data-animate="@if(isset($item_top->animation)){{$item_top->animation}}@endif" data-delay="@if(isset($item_top->animationDelay)){{$item_top->animationDelay}}@endif">
+        <h2>{{ $item_top->title }}<span>{{ $item_top->subtitle }}</span></h2>
+        <span>{{ $item_top->content }}</span>
     </div>
 
-    @foreach($items_spec as $item )
+    @foreach($items as $item )
         <div class="col_one_third nobottommargin @if($loop->last) col_last @endif">
             <div class="feature-box media-box" data-animate="@if(isset( $item->animation )){{ $item->animation }}@endif" data-delay="@if(isset( $item->animationDelay )){{ $item->animationDelay }}@endif">
                 <div class="fbox-media">
@@ -13,7 +13,7 @@
                 </div>
                 <div class="fbox-desc">
                     <h3>{{ $item->title }}<span class="subtitle">{{ $item->subtitle }}</span></h3>
-                    {!! $item->content !!}
+                    <p>{{ $item->content }}</p>
                 </div>
             </div>
         </div>

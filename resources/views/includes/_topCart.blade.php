1ebc1dd895500c76794e78e530adb70ce68d6f31
Lin Pin Chueh <javck.zack@gmail.com>
2019-09-28 15:26:22 +0800

    bug fix

--- resources/views/includes/_topCart.blade.php
+++ resources/views/includes/_topCart.blade.php
@@ -1,21 +1,24 @@
 <!-- include前，外部請先包以下程式碼
 <div id="top-cart"> ... </div>
 -->
-<a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span>{{Cart::count()}}</span></a>
+<a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span>{{Cart::getTotalQuantity()}}</span></a>
 <div class="top-cart-content">
     <div class="top-cart-title">
         <h4>{{trans('page.shopCart')}}</h4>
     </div>
     <div class="top-cart-items">
-        @forelse(Cart::content() as $cartItem)
+        @forelse(Cart::getContent() as $cartItem)
             <div class="top-cart-item clearfix">
                 <div class="top-cart-item-image">
-                    <a href="#"><img src="{{Voyager::image($cartItem->model->getFirstPic())}}" alt="{{$cartItem->model->title}}" /></a>
+                    @php
+                        $item = \App\Item::findOrFail($cartItem->id);
+                    @endphp
+                    <a href="#"><img src="{{Voyager::image($item->getFirstPic())}}" alt="{{$cartItem->name}}" /></a>
                 </div>
                 <div class="top-cart-item-desc">
-                    <a href="#">{{$cartItem->model->title}}</a>
-                    <span class="top-cart-item-price">${{$cartItem->model->price_new or $cartItem->model->price_og}}</span>
-                    <span class="top-cart-item-quantity">x {{$cartItem->qty}}</span>
+                    <a href="#">{{$cartItem->name}}</a>
+                    <span class="top-cart-item-price">${{$cartItem->price}}</span>
+                    <span class="top-cart-item-quantity">x {{$cartItem->quantity}}</span>
                 </div>
             </div>
         @empty
@@ -23,35 +26,10 @@
         @endforelse
     </div>
     <div class="top-cart-action clearfix">
-        <span class="fleft top-checkout-price">${{Cart::subtotal()}}</span>
-        @if(count(Cart::content())!=0)
-            <button class="button button-3d button-small nomargin" id="clear-cart" onclick="clearCart()">清空</button>
+        <span class="fleft top-checkout-price">${{Cart::getTotal()}}</span>
+        @if(Cart::getTotalQuantity()!=0)
             <button class="button button-3d button-small nomargin fright" onclick="location.href='{{url("shop/showCart")}}'">{{trans('page.seeShopCart')}}</button>
         @endif
         
     </div>
 </div>
-
-<script src="{{asset('js/jquery.js')}}"></script>
-<script>
-    function clearCart(){
-        var url = document.URL;
-        if (url.slice(-1) == '#'){
-            url = url.substr(0,url.length-1);
-        }
-        event.preventDefault();
-        var item_id = $(this).attr('href');
-        //console.log(url + 'api/shop/clearCart');
-        $.ajax({
-            type: "get",
-            url: url + 'api/shop/clearCart',
-            success: function (data) {
-                console.log(data);
-            },
-            error: function (data) {
-                console.log('Error:', data);
-            }
-        });
-    }
-
-</script>

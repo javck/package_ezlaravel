ff0387524249946859d197a486013917ce59cea6
林品爵 <javck.zack@gmail.com>
2020-01-29 19:01:42 +0800

    將Voyager升級到1.3.x

--- publishable/views/layouts/edit-add-master.blade.php
+++ publishable/views/layouts/edit-add-master.blade.php
@@ -256,16 +256,29 @@
         }
 
         $('document').ready(function () {
-            $('#slug').slugify();
             $('.toggleswitch').bootstrapToggle();
 
             //Init datepicker for date fields if data-datepicker attribute defined
             //or if browser does not handle date inputs
             $('.form-group input[type=date]').each(function (idx, elt) {
-                if (elt.type != 'date' || elt.hasAttribute('data-datepicker')) {
+                if (elt.hasAttribute('data-datepicker')) {
                     elt.type = 'text';
                     $(elt).datetimepicker($(elt).data('datepicker'));
+                } else if (elt.type != 'date') {
+                    elt.type = 'text';
+                    $(elt).datetimepicker({
+                        format: 'L',
+                        extraFormats: [ 'YYYY-MM-DD' ]
+                    }).datetimepicker($(elt).data('datepicker'));
                 }
+            });
+
+            @if ($isModelTranslatable)
+                $('.side-body').multilingual({"editing": true});
+            @endif
+
+            $('.side-body input[data-slug-origin]').each(function(i, el) {
+                $(el).slugify();
             });
 
             $('.form-group').on('click', '.remove-multi-image', deleteHandler('img', true));
@@ -273,41 +286,23 @@
             $('.form-group').on('click', '.remove-multi-file', deleteHandler('a', true));
             $('.form-group').on('click', '.remove-single-file', deleteHandler('a', false));
 
-        @if ($isModelTranslatable)
-            $('.side-body').multilingual({"editing": true});
-        @endif
-
-        $('.side-body input[data-slug-origin]').each(function(i, el) {
-               $(el).slugify();
-           });
-            $('.form-group').on('click', '.remove-multi-image', function (e) {
-               e.preventDefault();
-               $image = $(this).siblings('img');
-                params = {
-                   slug:   '{{ $dataType->slug }}',
-                   image:  $image.data('image'),
-                   id:     $image.data('id'),
-                   field:  $image.parent().data('field-name'),
-                   _token: '{{ csrf_token() }}'
-               }
-                $('.confirm_delete_name').text($image.data('image'));
-               $('#confirm_delete_modal').modal('show');
-           });
             $('#confirm_delete').on('click', function(){
-               $.post('{{ route('voyager.media.remove') }}', params, function (response) {
-                   if ( response
-                       && response.data
-                       && response.data.status
-                       && response.data.status == 200 ) {
+                $.post('{{ route('voyager.'.$dataType->slug.'.media.remove') }}', params, function (response) {
+                    if ( response
+                        && response.data
+                        && response.data.status
+                        && response.data.status == 200 ) {
+
                         toastr.success(response.data.message);
-                       $image.parent().fadeOut(300, function() { $(this).remove(); })
-                   } else {
-                       toastr.error("Error removing image.");
-                   }
-               });
+                        $file.parent().fadeOut(300, function() { $(this).remove(); })
+                    } else {
+                        toastr.error("Error removing file.");
+                    }
+                });
+
                 $('#confirm_delete_modal').modal('hide');
-           });
-           $('[data-toggle="tooltip"]').tooltip();
+            });
+            $('[data-toggle="tooltip"]').tooltip();
         });
     </script>
     @yield('javascript_child')
